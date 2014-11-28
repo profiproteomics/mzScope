46658c0f33b76aaec16f2b3e332ae6836683be75
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-11-28 14:24:39 +0000

    Add assembly to create zip distrib with batch files

---
+++ src/main/scripts/extract.bat
@@ -0,0 +1,2 @@
+java -cp "lib/*;mzScope-${pom.version}.jar;config" fr.profi.mzscope.ChromatogramsExporter %*
+pause   
---
+++ src/main/scripts/mzScope.bat
@@ -0,0 +1,2 @@
+java -cp "lib/*;mzScope-${pom.version}.jar;config" -jar mzScope-${pom.version}.jar
+pause   
