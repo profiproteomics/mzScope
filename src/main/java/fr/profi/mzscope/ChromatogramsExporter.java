9ec62ab799d82a83a59e03eaf86ffefd699fa9b5
Christophe Bruley <christophe.bruley@cea.fr>
2014-12-23 09:39:19 +0000

    unused import

--- src/main/java/fr/profi/mzscope/ChromatogramsExporter.java
+++ src/main/java/fr/profi/mzscope/ChromatogramsExporter.java
@@ -17,7 +17,6 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
