bd856eaaf43edc360d724bc3cb0a86d30e744035
Christophe Bruley <christophe.bruley@cea.fr>
2015-01-27 09:15:07 +0000

    - columns added - sort enhanced

--- src/main/java/fr/profi/mzscope/ui/FeaturesPanel.java
+++ src/main/java/fr/profi/mzscope/ui/FeaturesPanel.java
@@ -7,18 +7,32 @@
 package fr.profi.mzscope.ui;
 
 import fr.profi.mzdb.model.Feature;
+import java.awt.Rectangle;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.event.RowSorterEvent;
+import javax.swing.event.RowSorterListener;
+import javax.swing.table.TableColumnModel;
+import org.jdesktop.swingx.decorator.HighlighterFactory;
+import org.jdesktop.swingx.renderer.DefaultTableRenderer;
+import org.jdesktop.swingx.renderer.StringValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  *
  * @author CB205360
  */
-public class FeaturesPanel extends javax.swing.JPanel {
+public class FeaturesPanel extends javax.swing.JPanel implements RowSorterListener {
 
+   final private static Logger logger = LoggerFactory.getLogger(FeaturesPanel.class);
+   
    private IRawFilePlot rawFilePanel;
    private List<Feature> features = new ArrayList<Feature>();
-   
+   private int modelSelectedIdxBeforeSort = -1;
    /**
     * Creates new form FeaturesTable
     */
@@ -29,6 +43,22 @@ public class FeaturesPanel extends javax.swing.JPanel {
    public FeaturesPanel(IRawFilePlot rawFilePanel) {
       this.rawFilePanel = rawFilePanel;
       initComponents();
+      featureTable.getRowSorter().addRowSorterListener(this);
+      TableColumnModel columnModel = featureTable.getColumnModel();
+      for( int k = 0; k < columnModel.getColumnCount(); k++) { 
+         switch(FeaturesTableModel.Columns.values()[k]) {
+            case MZ_COL: 
+               columnModel.getColumn(k).setCellRenderer(new DefaultTableRenderer(new NumberFormatter("#.0000"), JLabel.RIGHT));
+               break;
+            case ET_COL: 
+            case DURATION_COL: 
+               columnModel.getColumn(k).setCellRenderer(new DefaultTableRenderer(new NumberFormatter("#0.00"), JLabel.RIGHT));   
+               break;
+            case APEX_INT_COL:
+            case AREA_COL: columnModel.getColumn(k).setCellRenderer(new DefaultTableRenderer(new NumberFormatter("#,###,###"), JLabel.RIGHT));
+               break;
+         }
+      }
    }
    
  
@@ -45,8 +75,11 @@ public class FeaturesPanel extends javax.swing.JPanel {
       featureTable = new org.jdesktop.swingx.JXTable();
 
       featureTable.setModel(featureTableModel);
+      featureTable.setColumnControlVisible(true);
       featureTable.setEditable(false);
       featureTable.setShowGrid(true);
+      featureTable.setShowHorizontalLines(false);
+      featureTable.addHighlighter(HighlighterFactory.createSimpleStriping());
       featureTable.addMouseListener(new java.awt.event.MouseAdapter() {
          public void mouseClicked(java.awt.event.MouseEvent evt) {
             featureTableMouseClicked(evt);
@@ -67,7 +100,8 @@ public class FeaturesPanel extends javax.swing.JPanel {
    }// </editor-fold>//GEN-END:initComponents
 
    private void featureTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_featureTableMouseClicked
-      if ((features != null) && (!features.isEmpty()) && (rawFilePanel != null) && (evt.getClickCount() == 2)) {
+      if ((features != null) && (!features.isEmpty()) && (rawFilePanel != null) 
+              && (evt.getClickCount() == 2) && (featureTable.getSelectedRow() != -1)) {
          Feature f = features.get(featureTable.convertRowIndexToModel(featureTable.getSelectedRow()));
          rawFilePanel.displayFeature(f);
          //propertySheetPanel.setBean(f); // TODO
@@ -85,5 +119,40 @@ public class FeaturesPanel extends javax.swing.JPanel {
    private fr.profi.mzscope.ui.FeaturesTableModel featureTableModel;
    private javax.swing.JScrollPane jScrollPane1;
    // End of variables declaration//GEN-END:variables
+
+   @Override
+   public void sorterChanged(RowSorterEvent e) {
+      if(featureTable.getSelectedRow() == -1)
+         return;
+      if (e.getType() == RowSorterEvent.Type.SORT_ORDER_CHANGED) {
+         modelSelectedIdxBeforeSort = featureTable.convertRowIndexToModel(featureTable.getSelectedRow());
+      } else if (modelSelectedIdxBeforeSort != -1) {
+         int idx = featureTable.convertRowIndexToView(modelSelectedIdxBeforeSort);
+          featureTable.scrollRectToVisible(new Rectangle(featureTable.getCellRect(idx, 0, true)));
+      }
+   }
+}
+
+class NumberFormatter implements StringValue {
+
+   private DecimalFormat format;
+   
+   public NumberFormatter(String pattern) {
+      this.format = new DecimalFormat(pattern);
+   }
+   
+   @Override
+   public String getString(Object value) {
+      if (value == null) {
+         return "";
+      }
+      try {
+         return format.format(value);
+      } catch (IllegalArgumentException e) {
+         // didn't work, nothing we can do
+      }
+      return value.toString();
+   }
+
 }
 
