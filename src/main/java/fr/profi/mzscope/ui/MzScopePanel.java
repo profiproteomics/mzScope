441b5c8260cc893410e7f266e19d1a0c9d1fd0bb
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-03-03 09:29:45 +0000

    mzdb filter moved

--- src/main/java/fr/profi/mzscope/ui/MzScopePanel.java
+++ src/main/java/fr/profi/mzscope/ui/MzScopePanel.java
@@ -456,23 +456,4 @@ public class MzScopePanel extends JPanel implements RawFileListener {
 
     
     
-    /**
-     * MZDB file filter class
-     */
-    public class MzdbFilter extends FileFilter {
-
-        @Override
-        public boolean accept(File file) {
-            // Allow only directories, or files with ".txt" extension
-            return file.isDirectory() || file.getAbsolutePath().endsWith(".mzdb");
-        }
-
-        @Override
-        public String getDescription() {
-            // This description will be displayed in the dialog,
-            // hard-coded = ugly, should be done via I18N
-            return "Mzdb file (*.mzdb)";
-        }
-
-    }
 }
