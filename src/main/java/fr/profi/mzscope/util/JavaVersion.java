abf7aeb2929f1fb6cc5853a2d1da9aa917b401a9
Christophe Delapierre <christophe.delapierre@cea.fr>
2023-09-22 16:34:28 +0200

    Graphic bug on SignalEditorPanel removed, minor changes on code

--- src/main/java/fr/profi/mzscope/util/JavaVersion.java
+++ src/main/java/fr/profi/mzscope/util/JavaVersion.java
@@ -9,7 +9,7 @@ public class JavaVersion implements IVersion {
     }
 
     public String getVersion() {
-	return "1.2.0-SNAPSHOT_2023-09-18T06:01:08Z_CD272832";
+	return "1.2.0-SNAPSHOT_2023-09-22T14:21:16Z_CD272832";
     }
 
 }
